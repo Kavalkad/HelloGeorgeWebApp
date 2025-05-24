1e89780c3d5c8669ba94bf52c83409208d183270
Kavalkad <github.andy2006@gmail.com>
2025-05-24 14:22:44 +0300

    Meet my first site

--- HelloGeorgeWebApp.Web/Program.cs
+++ HelloGeorgeWebApp.Web/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Mommy!");
+app.MapGet("/", () => "Hello George from Mr. Stark!");
 
 app.Run();
